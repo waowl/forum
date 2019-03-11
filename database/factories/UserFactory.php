4bfed7e8cd6ed282878825ba7caccb4f60ff77f0
Serj <henrick84@mail.ru>
2019-03-11 15:51:33 +0300

    5c8654b6a500f18a14077c36 Создать фабрики

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,24 @@ $factory->define(User::class, function (Faker $faker) {
 });
 
 
-$factory->define(\App\Thread::class, function (Faker $faker){
+$factory->define(\App\Thread::class, function (Faker $faker) {
     return [
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
 
+$factory->define(\App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(App\Thread::class)->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
