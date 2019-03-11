3a7bfa892b87ba27971b04292a208fbd46655281
Serj <henrick84@mail.ru>
2019-03-11 15:20:43 +0300

    5c864f330703354255039545 Создать миграции

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,3 +24,10 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+
+$factory->define(\App\Thread::class, function (Faker $faker){
+    return [
+
+    ];
+});
