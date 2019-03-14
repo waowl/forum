dab4ec6d73dde268687accd1fe94a1409d238c06
Serj <henrick84@mail.ru>
2019-03-14 16:11:25 +0300

    Create Thread Test

---
+++ tests/Feature/CreateThreadTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+
+    /**
+     * @test
+     *
+     * */
+    public function an_unauthenticated_user_may_not_create_a_thread()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = make(Thread::class);
+
+        $this->post('/thread', $thread->toArray());
+    }
+
+    /**
+     * @test
+     *
+     * */
+    public function a_user_may_create_a_thread()
+    {
+        $this->actingAs(create(User::class));
+
+        $thread = make(Thread::class);
+
+        $this->post('/thread', $thread->toArray());
+
+        $this->get('/thread')->assertSee($thread->title)->assertSee($thread->body);
+    }
+
+}
