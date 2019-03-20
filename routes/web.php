7d42863017efd3506f0bec1b3f0168802e13c9c7
Serj <henrick84@mail.ru>
2019-03-20 11:55:17 +0300

    filtering threads by channel

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/thread','ThreadController@index')->name('threads');
-Route::get('/thread/{channel}','ThreadController@index');
 Route::get('/thread/create','ThreadController@create')->name('thread.create');
+Route::get('/thread/{channel}','ThreadController@index');
 Route::post('/thread','ThreadController@store')->name('thread.store');
 Route::get('/thread/{channel}/{thread}','ThreadController@view')->name('thread');
 Route::post('/thread/{channel}/{thread}/reply','ReplyController@create');
