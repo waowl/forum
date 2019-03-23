f7a3b660c13d29d8368a542373786da19c73374f
Serj <henrick84@mail.ru>
2019-03-23 16:54:01 +0300

    test fix

---
+++ tests/Unit/FavoriteTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Reply;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+class FavoriteTest extends TestCase
+{
+    use DatabaseMigrations;
+    /** @test */
+    public function a_guest_cant_favorite_anything()
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $reply = create(Reply::class);
+        $this->post("/reply/{$reply->id}/favorite");
+
+    }
+
+    /** @test */
+    public function a_user_can_favorite_any_reply()
+    {
+        $this->signIn(create(User::class));
+        $reply = create(Reply::class);
+
+        $this->post("/reply/{$reply->id}/favorite");
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /** @test */
+    public function a_user_may_favorite_any_reply_only_once()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn(create(User::class));
+        $reply = create(Reply::class);
+
+        $this->post("/reply/{$reply->id}/favorite");
+        $this->post("/reply/{$reply->id}/favorite");
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
