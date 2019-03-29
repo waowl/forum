727e3db7ce5b869dfdbb9368dc9a237d3fd00638
Serj <henrick84@mail.ru>
2019-03-29 19:14:52 +0300

    added momentjs

--- resources/js/components/NewReply.vue
+++ resources/js/components/NewReply.vue
@@ -6,7 +6,7 @@
                                 <textarea name="body" id="body " cols="30" rows="6" class="form-control "
                                           placeholder="Leave your comment here ..." v-model="reply"></textarea>
             </div>
-            <button type="submit" class="btn btn-outline-primary" @click="addReply">Save</button>
+            <button type="submit" class="btn btn-outline-primary" @click="addReply" :disabled="sending">Save</button>
         </div>
         <div class="text-center" v-else>
             Please, <a href="/login">sing in</a> to participate in forum.
@@ -20,17 +20,20 @@
         data () {
             return {
                 reply: '',
-                path: this.thread_path + '/reply'
+                path: this.thread_path + '/reply',
+                sending:false
             }
         },
         methods: {
             addReply () {
                 if (!!this.reply) {
+                    this.sending = true
                     axios.post(this.path, {'body': this.reply})
                         .then(({data}) => {
                             this.reply = ''
                             this.$emit('added', data)
                             flash('Reply was added!')
+                            this.sending = false
                         })
                 }
             }
--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -1,18 +1,55 @@
+<template>
+    <div class="card mt-2" :id="'reply'+reply.id">
+        <div class="card-header d-flex flex-row justify-content-between">
+            <div>
+                <a :href="'/profiles/'+reply.owner.name" v-text="reply.owner.name"> </a>
+                said <span v-text="ago"></span>
+            </div>
+           <div v-if="signedIn">
+               <favorite :reply="reply"></favorite>
+           </div>
+        </div>
+        <div class="card-body">
+            <div v-if="editing">
+                <textarea name="" id="" cols="30" rows="10" class="form-control" v-model="body"></textarea>
+                <div class="d-flex flex-row justify-content-start">
+                    <button class="btn bg-primary text-white" @click="update">Save</button>
+                    <button class="btn bg-danger text-white" @click="editing=false">Close</button>
+                </div>
+            </div>
+            <div v-else v-text="this.body">
+            </div>
+            <hr>
+            <div class="d-flex flex-row justify-content-end" v-if="canUpdate">
+
+                <button class="btn btn-link  text-danger " @click="destroy"><i class="far fa-trash-alt"></i> Delete
+                </button>
+                <button @click="editing = !editing" class="btn btn-link text-success"><i class="far fa-edit"></i>Edit
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
 <script>
+    import  moment from 'moment'
     import Favorite from  './Favorite'
     export default {
-        props: ['attributes'],
+        components: {
+            Favorite
+        },
+        props: ['data'],
         data () {
             return {
-                body: this.attributes.body,
-                editing: false
+                reply: this.data,
+                body: this.data.body,
+                editing: false,
             }
         },
 
         methods: {
             update () {
                 if (this.body) {
-                    axios.patch(`/reply/${this.attributes.id}`, {'body': this.body})
+                    axios.patch(`/reply/${this.data.id}`, {'body': this.body})
                         .then((res) => {
                             this.editing = false
                             flash(res.data.status)
@@ -21,7 +58,7 @@
                 }
             },
             destroy () {
-                axios.delete(`/reply/${this.attributes.id}`)
+                axios.delete(`/reply/${this.data.id}`)
                     .then((res) => {
                         this.$emit('deleted', this.data.id)
                         flash(res.data.status)
@@ -32,6 +69,9 @@
         computed: {
             signedIn(){
                 return window.App.signedIn;
+            },
+            ago() {
+              return moment(this.reply.created_at).fromNow()
             },
             canUpdate(){
                 if( window.App.user) {
