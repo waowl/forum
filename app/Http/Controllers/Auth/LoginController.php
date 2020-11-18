46436dd7201e5cc4eacfebc622d5a8710bdab5dd
serhei_s <serjsenjob@gmail.com>
2020-11-18 17:48:21 +0300

    github auth added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\SocialAccount;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
@@ -43,9 +46,61 @@ class LoginController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
-    public function  handleProviderCallback(string $provider)
+    public function handleProviderCallback(string $provider)
     {
-        $user = Socialite::driver($provider)->user();
-        dd($user);
+        $socialiteUser = Socialite::driver($provider)->user();
+
+        $user = $this->findOrCreateUser($provider, $socialiteUser);
+
+        auth()->login($user, true);
+
+        return redirect('/');
+    }
+
+    public function findOrCreateUser($provider, $socialiteUser)
+    {
+        if ($user = $this->findUserBySocialId($provider, $socialiteUser->getId())) {
+            return $user;
+        }
+
+        if ($user = $this->findUserByEmail($provider, $socialiteUser->getEmail())) {
+            $this->addSocialAccount($provider, $user, $socialiteUser);
+
+            return $user;
+        }
+
+        $user = User::create([
+            'name' => $socialiteUser->getName(),
+            'email' => $socialiteUser->getEmail(),
+            'password' => bcrypt(Str::random(25)),
+        ]);
+
+        $this->addSocialAccount($provider, $user, $socialiteUser);
+
+        return $user;
+    }
+
+    public function findUserBySocialId($provider, $id)
+    {
+        $socialAccount = SocialAccount::where('provider', $provider)
+            ->where('provider_id', $id)
+            ->first();
+
+        return $socialAccount ? $socialAccount->user : false;
+    }
+
+    public function findUserByEmail($provider, $email)
+    {
+        return !$email ? null : User::where('email', $email)->first();
+    }
+
+    public function addSocialAccount($provider, $user, $socialiteUser)
+    {
+        SocialAccount::create([
+            'user_id' => $user->id,
+            'provider' => $provider,
+            'provider_id' => $socialiteUser->getId(),
+            'token' => $socialiteUser->token,
+        ]);
     }
 }
